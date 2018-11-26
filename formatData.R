eba8e69a267f07871832626c86b3ce998b472dea
Hiroyuki Kuromiya <khiroyuki1993@gmail.com>
2018-11-26 16:43:55 +0900

    make structured data

--- formatData.R
+++ formatData.R
@@ -1,3 +1,10 @@
 library(tidyverse)
 
-data = read_csv("./data/KU/Course_24a65f29b6_EventStream.csv")
+events = read_csv("./data/KyoU/Course_509a6f75849b_EventStream.csv")
+scores = read_csv("./data/KyoU/Course_509a6f75849b_QuizScore.csv")
+
+scores %>% ggplot(aes(score)) + geom_histogram()
+events.tidy = events %>% select(userid, operationname) %>% gather(operationname, key = "action", value = "cnt")
+events.structured = as.data.frame.matrix(table(events.tidy %>% select(-action))) %>% mutate(userid = rownames(events.structured))
+data = events.structured %>% left_join(scores, by = "userid") %>% select(-userid)
+cor(data)
